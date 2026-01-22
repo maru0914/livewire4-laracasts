eee56669c15242ae22f11d2aa4132bde58a4afcf
maru0914 <56859729+maru0914@users.noreply.github.com>
2026-01-22 21:26:16 +0900

    install flux

--- resources/views/pages/post/⚡create.blade.php
+++ resources/views/pages/post/⚡create.blade.php
@@ -1,13 +1,30 @@
 <?php
 
+use App\Models\Post;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
-new class extends Component
-{
-    //
+new #[Layout('layouts.app', ['title' => 'Create Post'])]
+class extends Component {
+    public string $title = '';
+    public string $content = '';
+
+    public function save(): void
+    {
+        Post::create($this->validate([
+            'title' => 'required|min:3',
+            'content' => 'required'
+        ]));
+
+        $this->redirect('/');
+    }
 };
 ?>
 
 <div>
-    {{-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius --}}
+    <form wire:submit="save" class="w-96 space-y-6">
+        <flux:input wire:model="title" label="Title" />
+        <flux:textarea wire:model="content" label="Content" />
+        <flux:button variant="primary">Save</flux:button>
+    </form>
 </div>
